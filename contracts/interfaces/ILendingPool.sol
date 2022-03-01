954deaf419fb18d8ef7b96f26f20f9a3fe154527
brianle83 <ble8330@gmail.com>
2022-03-01 15:46:45 -0800

    refactor: import openzeppelin and solmate, change styling

--- contracts/interfaces/ILendingPool.sol
+++ contracts/interfaces/ILendingPool.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 pragma solidity 0.8.11;
 
 /**
@@ -6,16 +8,16 @@ pragma solidity 0.8.11;
  **/
 interface ILendingPool {
     /**
-    * @dev Deposits an `amount` of underlying asset into the reserve, receiving in return overlying aTokens.
-    * - E.g. User deposits 100 USDC and gets in return 100 aUSDC
-    * @param asset The address of the underlying asset to deposit
-    * @param amount The amount to be deposited
-    * @param onBehalfOf The address that will receive the aTokens, same as msg.sender if the user
-    *   wants to receive them on his own wallet, or a different address if the beneficiary of aTokens
-    *   is a different wallet
-    * @param referralCode Code used to register the integrator originating the operation, for potential rewards.
-    *   0 if the action is executed directly by the user, without any middle-man
-    **/
+     * @dev Deposits an `amount` of underlying asset into the reserve, receiving in return overlying aTokens.
+     * - E.g. User deposits 100 USDC and gets in return 100 aUSDC
+     * @param asset The address of the underlying asset to deposit
+     * @param amount The amount to be deposited
+     * @param onBehalfOf The address that will receive the aTokens, same as msg.sender if the user
+     *   wants to receive them on his own wallet, or a different address if the beneficiary of aTokens
+     *   is a different wallet
+     * @param referralCode Code used to register the integrator originating the operation, for potential rewards.
+     *   0 if the action is executed directly by the user, without any middle-man
+     **/
     function deposit(
         address asset,
         uint256 amount,
@@ -24,27 +26,27 @@ interface ILendingPool {
     ) external;
 
     /**
-    * @dev Withdraws an `amount` of underlying asset from the reserve, burning the equivalent aTokens owned
-    * E.g. User has 100 aUSDC, calls withdraw() and receives 100 USDC, burning the 100 aUSDC
-    * @param asset The address of the underlying asset to withdraw
-    * @param amount The underlying amount to be withdrawn
-    *   - Send the value type(uint256).max in order to withdraw the whole aToken balance
-    * @param to Address that will receive the underlying, same as msg.sender if the user
-    *   wants to receive it on his own wallet, or a different address if the beneficiary is a
-    *   different wallet
-    * @return The final amount withdrawn
-    **/
+     * @dev Withdraws an `amount` of underlying asset from the reserve, burning the equivalent aTokens owned
+     * E.g. User has 100 aUSDC, calls withdraw() and receives 100 USDC, burning the 100 aUSDC
+     * @param asset The address of the underlying asset to withdraw
+     * @param amount The underlying amount to be withdrawn
+     *   - Send the value type(uint256).max in order to withdraw the whole aToken balance
+     * @param to Address that will receive the underlying, same as msg.sender if the user
+     *   wants to receive it on his own wallet, or a different address if the beneficiary is a
+     *   different wallet
+     * @return The final amount withdrawn
+     **/
     function withdraw(
         address asset,
         uint256 amount,
         address to
     ) external returns (uint256);
-    
+
     /**
-    * @dev Returns the state and configuration of the reserve
-    * @param asset The address of the underlying asset of the reserve
-    **/
-    function getReserveData(address asset) 
+     * @dev Returns the state and configuration of the reserve
+     * @param asset The address of the underlying asset of the reserve
+     **/
+    function getReserveData(address asset)
         external
         view
         returns (
