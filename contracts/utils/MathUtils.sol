4810a58ae0a002f9011bcd9898fdb648fe68e49c
brianle83 <ble8330@gmail.com>
2022-03-11 01:34:11 -0800

    refactor: remove unused function

--- contracts/utils/MathUtils.sol
+++ contracts/utils/MathUtils.sol
@@ -11,25 +11,6 @@ library MathUtils {
     }
 
     // From Solmate's FixedPointMathLib
-    function fmul(
-        uint256 x,
-        uint256 y,
-        uint256 baseUnit
-    ) internal pure returns (uint256 z) {
-        assembly {
-            // Store x * y in z for now.
-            z := mul(x, y)
-
-            // Equivalent to require(x == 0 || (x * y) / x == y)
-            if iszero(or(iszero(x), eq(div(z, x), y))) {
-                revert(0, 0)
-            }
-
-            // If baseUnit is zero this will return zero instead of reverting.
-            z := div(z, baseUnit)
-        }
-    }
-
     function mulDivDown(
         uint256 x,
         uint256 y,
