6256c3ebb750c914b857d84db5f1cfdcd950a25e
brianle83 <ble8330@gmail.com>
2022-03-12 22:06:37 -0800

    test: add test for sweep

--- tests/AaveStablecoinCellar.test.js
+++ tests/AaveStablecoinCellar.test.js
@@ -19,6 +19,7 @@ describe("AaveStablecoinCellar", () => {
   let owner;
   let alice;
   let cellar;
+  let Token;
   let usdc;
   let weth;
   let dai;
@@ -41,7 +42,7 @@ describe("AaveStablecoinCellar", () => {
     await router.deployed();
 
     // Deploy mock tokens
-    const Token = await ethers.getContractFactory("MockToken");
+    Token = await ethers.getContractFactory("MockToken");
     usdc = await Token.deploy("USDC");
     dai = await Token.deploy("DAI");
     weth = await Token.deploy("WETH");
@@ -107,6 +108,7 @@ describe("AaveStablecoinCellar", () => {
       stkAAVE.address,
       aave.address,
       weth.address,
+      usdc.address,
       usdc.address,
       "Sommelier Aave Stablecoin Cellar LP Token",
       "SASCT"
@@ -663,6 +665,41 @@ describe("AaveStablecoinCellar", () => {
     it("should allow deposits above max liquidity once restriction removed", async () => {
       await cellar.removeLiquidityRestriction();
       await cellar["deposit(uint256)"](1);
+    });
+  });
+
+  describe("sweep", () => {
+    let SOMM;
+
+    beforeEach(async () => {
+      SOMM = await Token.deploy("SOMM");
+      await SOMM.deployed();
+
+      // mimic 1000 SOMM being transferred to the cellar contract by accident
+      await SOMM.mint(cellar.address, 1000);
+    });
+
+    it("should not allow assets managed by cellar to be transferred out", async () => {
+      await expect(cellar.sweep(usdc.address)).to.be.revertedWith(
+        "ProtectedAsset()"
+      );
+      await expect(cellar.sweep(aUSDC.address)).to.be.revertedWith(
+        "ProtectedAsset()"
+      );
+    });
+
+    it("should recover tokens accidentally transferred to the contract", async () => {
+      await cellar.sweep(SOMM.address);
+
+      // expect 1000 SOMM to have been transferred from cellar to owner
+      expect(await SOMM.balanceOf(owner.address)).to.eq(1000);
+      expect(await SOMM.balanceOf(cellar.address)).to.eq(0);
+    });
+
+    it("should emit Sweep event", async () => {
+      await expect(cellar.sweep(SOMM.address))
+        .to.emit(cellar, "Sweep")
+        .withArgs(SOMM.address, 1000);
     });
   });
 });
