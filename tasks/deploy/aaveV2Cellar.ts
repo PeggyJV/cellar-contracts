3979615f69d440409e1572e0d4bdd6759aa84d9e
brianle83 <ble8330@gmail.com>
2022-04-07 11:53:16 -0700

    build(deploy): update deploy script

--- tasks/deploy/aaveV2Cellar.ts
+++ tasks/deploy/aaveV2Cellar.ts
@@ -18,13 +18,14 @@ task("deploy:AaveV2StablecoinCellar").setAction(async function (
 
   const cellar = <AaveV2StablecoinCellar>await factory.deploy(
     "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
-    "0xE592427A0AEce92De3Edee1F18E0157C05861564", // Uniswap Router
+    "0x8e764bE4288B842791989DB5b8ec067279829809", // Curve registry exchange
     "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F", // Sushiswap Router
     "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9", // Aave V2 Lending Pool
     "0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5", // Aave Incentives Controller V2
     "0x69592e6f9d21989a043646fE8225da2600e5A0f7", // Cosmos Gravity Bridge
     "0x4da27a545c0c5B758a6BA100e3a049001de870f5", // stkAAVE
-    "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9" // AAVE
+    "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", // AAVE
+    "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" // WETH
   );
 
   await cellar.deployed();
