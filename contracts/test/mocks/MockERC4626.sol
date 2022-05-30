d8216026c05edd78aedf8ba9d2f9848ad41968b3
brianle83 <ble8330@gmail.com>
2022-05-30 13:14:26 -0700

    feat: update to Solidity version 0.8.13

--- contracts/test/mocks/MockERC4626.sol
+++ contracts/test/mocks/MockERC4626.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.11;
+pragma solidity 0.8.13;
 
 import { ERC4626 } from "../../base/ERC4626.sol";
 import { ERC20 } from "@rari-capital/solmate/src/tokens/ERC20.sol";
