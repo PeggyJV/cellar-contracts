6e84f0c0660e55974f204b0e6230f77f65565847
brianle83 <0xble@pm.me>
2022-06-22 20:23:25 -0700

    build: update to solidity version 0.8.15

--- src/mocks/MockAToken.sol
+++ src/mocks/MockAToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.13;
+pragma solidity 0.8.15;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { MockLendingPool } from "./MockLendingPool.sol";
