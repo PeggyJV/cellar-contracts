efef140a449e00da350a84a2cf3b9407dee2903a
brianle83 <0xble@pm.me>
2022-06-27 11:27:22 -0700

    refactor(Cellar): merge enterPosition and exitPosition into rebalance, add high watermark fee accounting

--- src/mocks/MockCellar.sol
+++ src/mocks/MockCellar.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.15;
 
-import { Cellar, Registry, ERC4626, ERC20 } from "src/base/Cellar.sol";
+import { Cellar, Registry, ERC4626, ERC20, SafeCast } from "src/base/Cellar.sol";
 import { Test, console } from "@forge-std/Test.sol";
 
 contract MockCellar is Cellar, Test {
+    using SafeCast for uint256;
+    using SafeCast for int256;
+
     constructor(
         Registry _registry,
         ERC20 _asset,
@@ -13,13 +16,32 @@ contract MockCellar is Cellar, Test {
         string memory _symbol
     ) Cellar(_registry, _asset, _positions, _name, _symbol) {}
 
-    function increasePositionBalance(address position, uint256 amount) external {
+    function depositIntoPosition(
+        address position,
+        uint256 amount,
+        address mintSharesTo
+    ) external returns (uint256 shares) {
+        uint256 amountInAssets = registry.priceRouter().getValue(ERC4626(position).asset(), amount, asset);
+        shares = previewDeposit(amountInAssets);
+
         deal(address(ERC4626(position).asset()), address(this), amount);
 
-        // Update position balance.
-        getPositionData[position].balance += amount;
+        getPositionData[position].highWatermark += amount.toInt256();
 
-        // Deposit into position.
+        ERC4626(position).deposit(amount, address(this));
+
+        _mint(mintSharesTo, shares);
+    }
+
+    function depositIntoPosition(address position, uint256 amount) public returns (uint256 shares) {
+        uint256 amountInAssets = registry.priceRouter().getValue(ERC4626(position).asset(), amount, asset);
+        shares = previewDeposit(amountInAssets);
+
+        deal(address(ERC4626(position).asset()), address(this), amount);
+
+        getPositionData[position].highWatermark += amount.toInt256();
+        totalSupply += shares;
+
         ERC4626(position).deposit(amount, address(this));
     }
 }
