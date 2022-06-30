503c680eaff39cf111eb9144a96e62b9e48b1a56
zpoken <82594516+zpoken@users.noreply.github.com>
2022-06-30 09:43:03 +0300

    Registry: add events

--- src/Registry.sol
+++ src/Registry.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.15;
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 // TODO: configure defaults
-// TODO: add events
 
 contract Registry is Ownable {
     address public swapRouter;
@@ -12,11 +11,44 @@ contract Registry is Ownable {
     address public priceRouter;
 
     /**
+     * @notice Emitted when the swap router is changed.
+     * @param oldSwapRouter address of SwapRouter contract was changed from
+     * @param newSwapRouter address of SwapRouter contract was changed to
+     */
+    event SwapRouterChanged(
+        address oldSwapRouter,
+        address newSwapRouter
+    );
+
+    /**
+     * @notice Emitted when the Gravity Bridge is changed.
+     * @param oldGravityBridge address of GravityBridge contract was changed from
+     * @param newGravityBridge address of GravityBridge contract was changed to
+     */
+    event GravityBridgeChanged(
+        address oldGravityBridge,
+        address newGravityBridge
+    );
+
+    /**
+     * @notice Emitted when the price router is changed.
+     * @param oldPriceRouter address of PriceRouter contract was changed from
+     * @param newPriceRouter address of PriceRouter contract was changed to
+     */
+    event PriceRouterChanged(
+        address oldPriceRouter,
+        address newPriceRouter
+    );
+
+    /**
      * @notice Sets new address of SwapRouter contract.
      * @param newSwapRouter new SwapRouter address
      */
     function setSwapRouter(address newSwapRouter) external onlyOwner {
+        address oldSwapRouter = swapRouter;
         swapRouter = newSwapRouter;
+        
+        emit SwapRouterChanged(oldSwapRouter, newSwapRouter);
     }
 
     /**
@@ -24,7 +56,10 @@ contract Registry is Ownable {
      * @param newGravityBridge new GravityBridge address
      */
     function setGravityBridge(address newGravityBridge) external onlyOwner {
+        address oldGravityBridge = gravityBridge;
         gravityBridge = newGravityBridge;
+
+        emit GravityBridgeChanged(oldGravityBridge, newGravityBridge);
     }
 
     /**
@@ -32,6 +67,9 @@ contract Registry is Ownable {
      * @param newPriceRouter new PriceRouter address
      */
     function setPriceRouter(address newPriceRouter) external onlyOwner {
+        address oldPriceRouter = priceRouter;
         priceRouter = newPriceRouter;
+
+        emit PriceRouterChanged(oldPriceRouter, newPriceRouter);
     }
 }
