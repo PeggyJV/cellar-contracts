b38a69ec6c863a01ef4aaff45e5a64dc265d5cd6
zpoken <82594516+zpoken@users.noreply.github.com>
2022-06-30 09:20:00 +0300

    Registry: add natspec

--- src/Registry.sol
+++ src/Registry.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.15;
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 // TODO: configure defaults
-// TODO: add natspec
 // TODO: add events
 
 contract Registry is Ownable {
@@ -12,14 +11,26 @@ contract Registry is Ownable {
     address public gravityBridge;
     address public priceRouter;
 
+    /**
+     * @notice Sets new address of SwapRouter contract.
+     * @param newSwapRouter new SwapRouter address
+     */
     function setSwapRouter(address newSwapRouter) external onlyOwner {
         swapRouter = newSwapRouter;
     }
 
+    /**
+     * @notice Sets new address of GravityBridge contract.
+     * @param newGravityBridge new GravityBridge address
+     */
     function setGravityBridge(address newGravityBridge) external onlyOwner {
         gravityBridge = newGravityBridge;
     }
 
+    /**
+     * @notice Sets new address of PriceRouter contract.
+     * @param newPriceRouter new PriceRouter address
+     */
     function setPriceRouter(address newPriceRouter) external onlyOwner {
         priceRouter = newPriceRouter;
     }
