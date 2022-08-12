f15a972cfc732fd53a0e598569da199dbea602a3
crispymangoes <crispymangoes@gmail.com>
2022-08-12 10:23:28 -0700

    docs(ChainlinkProceFeedAdaptor): add natspec

--- src/modules/price-router/adaptors/ChainlinkPriceFeedAdaptor.sol
+++ src/modules/price-router/adaptors/ChainlinkPriceFeedAdaptor.sol
@@ -10,10 +10,6 @@ import { Denominations } from "@chainlink/contracts/src/v0.8/Denominations.sol";
 import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import { Math } from "src/utils/Math.sol";
 
-import { Test, console } from "@forge-std/Test.sol";
-
-import "src/Errors.sol";
-
 contract ChainlinkPriceFeedAdaptor {
     using SafeCast for int256;
     using Math for uint256;
@@ -26,6 +22,19 @@ contract ChainlinkPriceFeedAdaptor {
 
     // =========================================== HELPER FUNCTIONS ===========================================
 
+    /**
+     * @notice Could not find an asset's price in USD or ETH.
+     * @param asset address of the asset
+     */
+    error ChainlinkPriceFeedAdaptor__PriceNotAvailable(address asset);
+
+    /**
+     * @notice Interacts with Chainlink feed registry and first tries to get `asset` price in USD,
+     *         if that fails, then it tries to get `asset` price in ETH, and then converts the answer into USD.
+     * @param asset the ERC20 token to get the price of.
+     * @return price the price of `asset` in USD
+     * @return timestamp the last timestamp the price was updated by Chainlink nodes
+     */
     function _getValueInUSDAndTimestamp(ERC20 asset) internal view returns (uint256 price, uint256 timestamp) {
         try feedRegistry.latestRoundData(address(asset), Denominations.USD) returns (
             uint80,
@@ -49,11 +58,24 @@ contract ChainlinkPriceFeedAdaptor {
                 price = _price.toUint256().mulWadDown(_getExchangeRateFromETHToUSD());
                 timestamp = _timestamp;
             } catch {
-                revert STATE_PriceNotAvailable(address(asset));
+                revert ChainlinkPriceFeedAdaptor__PriceNotAvailable(address(asset));
             }
         }
     }
 
+    /**
+     * @notice Could not find an asset's price range in USD or ETH.
+     * @param asset address of the asset
+     */
+    error ChainlinkPriceFeedAdaptor__PriceRangeNotAvailable(address asset);
+
+    /**
+     * @notice Interacts with Chainlink feed registry and first tries to get `asset` price range in USD,
+     *         if that fails, then it tries to get `asset` price range in ETH, and then converts the range into USD.
+     * @param asset the ERC20 token to get the price range of.
+     * @return min the minimum price where Chainlink nodes stop updating the oracle
+     * @return max the maximum price where Chainlink nodes stop updating the oracle
+     */
     function _getPriceRangeInUSD(ERC20 asset) internal view returns (uint256 min, uint256 max) {
         try feedRegistry.getFeed(address(asset), Denominations.USD) returns (AggregatorV2V3Interface aggregator) {
             IChainlinkAggregator chainlinkAggregator = IChainlinkAggregator(address(aggregator));
@@ -73,11 +95,15 @@ contract ChainlinkPriceFeedAdaptor {
                 min = min.mulWadDown(exchangeRateFromETHToUSD);
                 max = max.mulWadDown(exchangeRateFromETHToUSD);
             } catch {
-                revert STATE_PriceRangeNotAvailable(address(asset));
+                revert ChainlinkPriceFeedAdaptor__PriceRangeNotAvailable(address(asset));
             }
         }
     }
 
+    /**
+     * @notice helper function to grab pricing data for ETH in USD
+     * @return exchangeRate the exchange rate for ETH in terms of USD
+     */
     function _getExchangeRateFromETHToUSD() internal view returns (uint256 exchangeRate) {
         exchangeRate = uint256(feedRegistry.latestAnswer(Denominations.ETH, Denominations.USD));
     }
