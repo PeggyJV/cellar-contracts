3ce7ac83db6c06cccca5879b5c0a8d7c2add890f
cookiesanddudes <cookies_dude@proton.me>
2022-10-29 17:43:53 +0100

    fix interface

--- src/interfaces/external/ICurvePool.sol
+++ src/interfaces/external/ICurvePool.sol
@@ -12,5 +12,5 @@ interface ICurvePool {
     function remove_liquidity(uint256 _amount, uint256[3] calldata min_amounts) external returns (uint256[3] memory); 
     function remove_liquidity_imbalance(uint256[3] calldata amounts, uint256 max_burn_amount) external; 
     function remove_liquidity_one_coin(uint256 _token_amount, int128 i, uint256 min_amount) external;
-    function calc_withdraw_one_coin(uint256, int128) external view returns (uint256);
+    function calc_withdraw_one_coin(uint256 token_amount, int128 i) external view returns (uint256);
 }
