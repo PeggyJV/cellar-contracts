51735b543ce74b3c8e7e60dc6afd567f362660cf
mehdi <rhouzm@gmail.com>
2022-10-29 16:03:58 +0100

    deposit

--- src/modules/adaptors/Compound/CompoundTokenAdaptor.sol
+++ src/modules/adaptors/Compound/CompoundTokenAdaptor.sol
@@ -20,6 +20,34 @@ abstract contract CToken is IERC20Upgradeable {
     function exchangeRateStored() external view virtual returns (uint256);
 }
 
+interface CTokenInterface {
+    /// @notice cToken is convertible into an ever increasing quantity of the underlying asset, as interest accrues in
+    /// the market. This function returns the exchange rate between a cToken and the underlying asset.
+    /// @dev returns the current exchange rate as an uint, scaled by 1 * 10^(18 - 8 + Underlying Token Decimals).
+    function exchangeRateCurrent() external returns (uint256);
+
+    function exchangeRateStored() external view returns (uint256);
+
+    function decimals() external returns (uint256);
+
+    function underlying() external view returns (address);
+
+    /// The mint function transfers an asset into the protocol, which begins accumulating interest based
+    /// on the current Supply Rate for the asset. The user receives a quantity of cTokens equal to the
+    /// underlying tokens supplied, divided by the current Exchange Rate.
+    /// @param mintAmount The amount of the asset to be supplied, in units of the underlying asset.
+    /// @return 0 on success, otherwise an Error code
+    function mint(uint256 mintAmount) external returns (uint256);
+
+    /// The redeem function converts a specified quantity of cTokens into the underlying asset, and returns
+    /// them to the user. The amount of underlying tokens received is equal to the quantity of cTokens redeemed,
+    /// multiplied by the current Exchange Rate. The amount redeemed must be less than the user's Account Liquidity
+    /// and the market's available liquidity.
+    /// @param redeemTokens The number of cTokens to be redeemed.
+    /// @return 0 on success, otherwise an Error code
+    function redeem(uint256 redeemTokens) external returns (uint256);
+}
+
 contract CErc20Storage {
     /**
      * @notice Underlying asset for this CToken
@@ -94,9 +122,9 @@ contract CompoundTokenAdapter is BaseAdaptor {
      * @dev configurationData is NOT used because this action will only increase the health factor
      */
 
-    /// @notice Underlying token address getter that must be overriden by child contracts
-    function underlying() external view virtual returns (address) {
-        return 
+    function underlying(bytes memory adaptorData) public view override returns (address) {
+        address target = abi.decode(adaptorData, (address));
+        return isCETH(target) ? WETH : CTokenInterface(target).underlying();
     }
 
 
@@ -106,8 +134,9 @@ contract CompoundTokenAdapter is BaseAdaptor {
         bytes memory configurationData
     ) public override {
 
-        IERC20Upgradeable cToken = IERC20Upgradeable(abi.decode(adaptorData, (address)));
-        ERC20 token = assetOf(abi.encode(cToken)); 
+        IERC20Metadata cToken = IERC20Metadata(abi.decode(adaptorData, (address)));
+        IERC20Metadata token = IERC20Metadata(underlying());
+
         token.safeTransferFrom(msg.sender, address(this), assets); // pulls the underlying
 
         // --- WETH into ETH
@@ -154,5 +183,4 @@ contract CompoundTokenAdapter is BaseAdaptor {
         //Transfer assets to receiver
         ERC20(token.UNDERLYING_ASSET_ADDRESS()).safeTransfer(receiver, assets);
     }
-ed changes
 }
