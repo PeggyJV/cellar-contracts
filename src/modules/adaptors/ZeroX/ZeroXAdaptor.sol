eeabc4f6bc1c5e2b47ff62a2b6ad007c17b049ac
crispymangoes <crispymangoes@gmail.com>
2023-03-08 08:45:44 -0800

    Create PositionlessAdaptor that positionless adaptors can inherit from

--- src/modules/adaptors/ZeroX/ZeroXAdaptor.sol
+++ src/modules/adaptors/ZeroX/ZeroXAdaptor.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.16;
 
-import { BaseAdaptor, ERC20, SafeTransferLib } from "src/modules/adaptors/BaseAdaptor.sol";
+import { ERC20, SafeTransferLib } from "src/modules/adaptors/BaseAdaptor.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { PositionlessAdaptor } from "src/modules/adaptors/PositionlessAdaptor.sol";
 
 /**
  * @title 0x Adaptor
  * @notice Allows Cellars to swap with 0x.
  * @author crispymangoes
  */
-contract ZeroXAdaptor is BaseAdaptor {
+contract ZeroXAdaptor is PositionlessAdaptor {
     using SafeTransferLib for ERC20;
     using Address for address;
 
@@ -36,59 +37,6 @@ contract ZeroXAdaptor is BaseAdaptor {
      */
     function identifier() public pure override returns (bytes32) {
         return keccak256(abi.encode("0x Adaptor V 0.0"));
-    }
-
-    //============================================ Implement Base Functions ===========================================
-
-    /**
-     * @notice User deposits are NOT allowed.
-     */
-    function deposit(
-        uint256,
-        bytes memory,
-        bytes memory
-    ) public pure override {
-        revert BaseAdaptor__UserDepositsNotAllowed();
-    }
-
-    /**
-     * @notice User withdraws are NOT allowed.
-     */
-    function withdraw(
-        uint256,
-        address,
-        bytes memory,
-        bytes memory
-    ) public pure override {
-        revert BaseAdaptor__UserWithdrawsNotAllowed();
-    }
-
-    /**
-     * @notice There is no underlying position so return zero.
-     */
-    function withdrawableFrom(bytes memory, bytes memory) public pure override returns (uint256) {
-        return 0;
-    }
-
-    /**
-     * @notice There is no underlying position so return zero.
-     */
-    function balanceOf(bytes memory) public pure override returns (uint256) {
-        return 0;
-    }
-
-    /**
-     * @notice There is no underlying position so return zero address.
-     */
-    function assetOf(bytes memory) public pure override returns (ERC20) {
-        return ERC20(address(0));
-    }
-
-    /**
-     * @notice There is no underlying position so return false.
-     */
-    function isDebt() public pure override returns (bool) {
-        return false;
     }
 
     //============================================ Strategist Functions ===========================================
