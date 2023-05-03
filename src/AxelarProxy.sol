6a0b4dd1cd53f5fd2ff9db391e9a433175d3d2bf
crispymangoes <crispymangoes@gmail.com>
2023-05-03 12:50:41 -0700

    Update Proxy to include sourceAddress check if Somm is not source chain

--- src/AxelarProxy.sol
+++ src/AxelarProxy.sol
@@ -7,7 +7,20 @@ import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 contract AxelarProxy is AxelarExecutable {
     using Address for address;
 
-    constructor(address gateway_) AxelarExecutable(gateway_) {}
+    event LogicCallEvent(address target, bytes callData);
+
+    error AxelarProxy__NoTokens();
+    error AxelarProxy__WrongSource();
+    error AxelarProxy__WrongSender();
+
+    bytes32 public immutable sourceChainHash;
+    bytes32 public immutable sourceAddressHash;
+    bytes32 public constant SOMMELIER_CHAIN_HASH = keccak256(bytes("Sommelier"));
+
+    constructor(address gateway_, string memory sourceChain, string memory sourceAddress) AxelarExecutable(gateway_) {
+        sourceChainHash = keccak256(bytes(sourceChain));
+        sourceAddressHash = keccak256(bytes(sourceAddress));
+    }
 
     function _execute(
         string calldata sourceChain,
@@ -15,22 +28,30 @@ contract AxelarProxy is AxelarExecutable {
         bytes calldata payload
     ) internal override {
         // TODO this check might not be needed since `execute` calls `validateContractCallAndMint` on the gateway.
-        if (msg.sender != address(gateway)) revert("Not the gateway");
+        // if (msg.sender != address(gateway)) revert AxelarProxy__NotTheGateway();
+
         // Validate Source Chain
-        if (keccak256(bytes(sourceChain)) != keccak256(bytes("Sommelier"))) revert("Wrong source");
-        // Validate source address or maybe not
+        bytes32 _sourceChainHash = sourceChainHash;
+        if (keccak256(bytes(sourceChain)) != _sourceChainHash) revert AxelarProxy__WrongSource();
+
+        if (_sourceChainHash != SOMMELIER_CHAIN_HASH) {
+            // Validate Sender.
+            if (keccak256(bytes(sourceAddress)) != sourceAddressHash) revert AxelarProxy__WrongSender();
+        }
 
         (address target, bytes memory callData) = abi.decode(payload, (address, bytes));
         target.functionCall(callData);
+
+        emit LogicCallEvent(target, callData);
     }
 
     function _executeWithToken(
-        string calldata sourceChain,
-        string calldata sourceAddress,
-        bytes calldata payload,
-        string calldata tokenSymbol,
-        uint256 amount
-    ) internal override {
-        revert("No tokens");
+        string calldata,
+        string calldata,
+        bytes calldata,
+        string calldata,
+        uint256
+    ) internal pure override {
+        revert AxelarProxy__NoTokens();
     }
 }
