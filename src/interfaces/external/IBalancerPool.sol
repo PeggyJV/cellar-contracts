3c662b41bbed7e5f3e3b471c747e17c2620bd01b
crispymangoes <crispymangoes@gmail.com>
2023-06-05 11:21:51 -0700

    Clean up interface by removing functions we will never use.

--- src/interfaces/external/IBalancerPool.sol
+++ src/interfaces/external/IBalancerPool.sol
@@ -29,34 +29,4 @@ interface IBalancerPool {
     function decimals() external view returns (uint8);
 
     function joinPool(uint256 poolAmountOut, uint256[] calldata maxAmountsIn) external;
-
-    function swapExactAmountIn(
-        address tokenIn,
-        uint256 tokenAmountIn,
-        address tokenOut,
-        uint256 minAmountOut,
-        uint256 maxPrice
-    ) external returns (uint256 tokenAmountOut, uint256 spotPriceAfter);
-
-    function swapExactAmountOut(
-        address tokenIn,
-        uint256 maxAmountIn,
-        address tokenOut,
-        uint256 tokenAmountOut,
-        uint256 maxPrice
-    ) external returns (uint256 tokenAmountIn, uint256 spotPriceAfter);
-
-    function joinswapExternAmountIn(
-        address tokenIn,
-        uint256 tokenAmountIn,
-        uint256 minPoolAmountOut
-    ) external returns (uint256 poolAmountOut);
-
-    function exitPool(uint256 poolAmountIn, uint256[] calldata minAmountsOut) external;
-
-    function exitswapExternAmountOut(
-        address tokenOut,
-        uint256 tokenAmountOut,
-        uint256 maxPoolAmountIn
-    ) external returns (uint256 poolAmountIn);
 }
