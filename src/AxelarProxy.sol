17d82179f172a2c05ce431a53acbb698549b369d
crispymangoes <77207459+crispymangoes@users.noreply.github.com>
2023-06-05 11:08:13 -0700

    Feat/axelar proxy (#118)
    * Create Proxy contract
    
    * Create simple axelar proxy contract
    
    * Update Proxy to include sourceAddress check if Somm is not source chain
    
    * Create deploy script
    
    * Create send message script
    
    * Use axelar lib
    
    * Clean up Axelar Proxy
    
    * add pause functionality to proxy
    
    * Add natspec
    
    * Remove stop execution
    
    * Update docs for Axelar Proxy
    
    * Add natspec to MockSommelier contract
    
    * Add note about Axelar executable logic to Axelar Proxy.
    
    * Fix typo

---
+++ src/AxelarProxy.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.16;
+
+import { AxelarExecutable } from "lib/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+
+/**
+ * @title Axelar Proxy
+ * @notice Allows for Cellars deployed on L2s to be controlled by the Sommelier Chain using Axelar messages.
+ * @dev This contract will be deployed on some L2, then to run a Cellar on that L2,
+ *      deploy the Cellar, and make this contract the owner.
+ * @author crispymangoes
+ */
+contract AxelarProxy is AxelarExecutable {
+    using Address for address;
+
+    event LogicCallEvent(address target, bytes callData);
+
+    error AxelarProxy__WrongSource();
+    error AxelarProxy__NoTokens();
+
+    bytes32 public constant SOMMELIER_CHAIN_HASH = keccak256(bytes("sommelier"));
+
+    constructor(address gateway_) AxelarExecutable(gateway_) {}
+
+    /**
+     * @notice Execution logic.
+     * @dev Verifies message is from Sommelier, otherwise reverts.
+     * @dev Verifies message is a valid Axelar message, otherwise reverts.
+     *      See `AxelarExecutable.sol`.
+     */
+    function _execute(string calldata sourceChain, string calldata, bytes calldata payload) internal override {
+        // Validate Source Chain
+        if (keccak256(bytes(sourceChain)) != SOMMELIER_CHAIN_HASH) revert AxelarProxy__WrongSource();
+
+        // Execute function call.
+        (address target, bytes memory callData) = abi.decode(payload, (address, bytes));
+        target.functionCall(callData);
+
+        emit LogicCallEvent(target, callData);
+    }
+
+    /**
+     * @notice This contract is not setup to handle ERC20 tokens, so execution with token calls will revert.
+     */
+    function _executeWithToken(
+        string calldata,
+        string calldata,
+        bytes calldata,
+        string calldata,
+        uint256
+    ) internal pure override {
+        revert AxelarProxy__NoTokens();
+    }
+}
