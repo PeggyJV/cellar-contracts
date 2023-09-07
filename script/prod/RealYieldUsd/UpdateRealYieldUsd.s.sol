2f3ee360be44ae6f42d54c7ad5c73a2c13eb23d8
crispymangoes <crispymangoes@gmail.com>
2023-09-07 12:17:15 -0700

    Update Real Yield USD timelock

--- script/prod/RealYieldUsd/UpdateRealYieldUsd.s.sol
+++ script/prod/RealYieldUsd/UpdateRealYieldUsd.s.sol
@@ -5,6 +5,8 @@ import { Cellar, ERC4626, ERC20, SafeTransferLib, PriceRouter } from "src/base/C
 import { TimelockController } from "@openzeppelin/contracts/governance/TimelockController.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
+import { MainnetAddresses } from "test/resources/MainnetAddresses.sol";
+
 import "forge-std/Script.sol";
 import { Math } from "src/utils/Math.sol";
 
@@ -27,17 +29,21 @@ interface ICellar {
  *      `source .env && forge script script/prod/RealYieldUsd/UpdateRealYieldUsd.s.sol:UpdateRealYieldUsdScript --rpc-url $MAINNET_RPC_URL  --private-key $PRIVATE_KEY —optimize —optimizer-runs 200 --with-gas-price 25000000000 --verify --etherscan-api-key $ETHERSCAN_KEY --slow --broadcast`
  * @dev Optionally can change `--with-gas-price` to something more reasonable
  */
-contract UpdateRealYieldUsdScript is Script {
+contract UpdateRealYieldUsdScript is Script, MainnetAddresses {
     TimelockController private controller = TimelockController(payable(0xaDa78a5E01325B91Bc7879a63c309F7D54d42950));
 
     IRegistry private registry = IRegistry(0x2Cbd27E034FEE53f79b607430dA7771B22050741);
 
-    address public cellarAdaptor = 0x3B5CA5de4d808Cd793d3a7b3a731D3E67E707B27;
+    address public aaveV2DebtTokenAdaptor = 0xeC86ac06767e911f5FdE7cba5D97f082C0139C01;
+    address public aaveV3DebtTokenAdaptor = 0x6DEd49176a69bEBf8dC1a4Ea357faa555df188f7;
+    address public morphoV2DebtTokenAdaptor = 0x407D5489F201013EE6A6ca20fCcb05047C548138;
 
-    address public sDai = 0x83F20F44975D03b1b09e64809B757c47f942BEeA;
+    address public aaveV3ATokenAdaptor = 0x3184CBEa47eD519FA04A23c4207cD15b7545F1A6;
 
     function run() external {
-        uint256 numberOfCalls = 2;
+        // Borrow USDC, DAI, USDT from Aave V2, V3
+        // Supply sDAI as collateral
+        uint256 numberOfCalls = 1;
 
         address[] memory targets = new address[](numberOfCalls);
         for (uint256 i; i < numberOfCalls; ++i) targets[i] = address(registry);
@@ -46,8 +52,13 @@ contract UpdateRealYieldUsdScript is Script {
 
         bytes[] memory payloads = new bytes[](numberOfCalls);
         // Need to trustAdaptors
-        payloads[0] = abi.encodeWithSelector(IRegistry.trustAdaptor.selector, cellarAdaptor, 0, 0);
-        payloads[1] = abi.encodeWithSelector(IRegistry.trustPosition.selector, cellarAdaptor, abi.encode(sDai), 0, 0);
+        payloads[0] = abi.encodeWithSelector(
+            IRegistry.trustPosition.selector,
+            aaveV3ATokenAdaptor,
+            abi.encode(aV3sDAI),
+            0,
+            0
+        );
 
         bytes32 predecessor = hex"";
         bytes32 salt = hex"";
