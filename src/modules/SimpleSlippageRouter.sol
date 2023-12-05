4e10dd6656918e7b708ebb8f3fc16095fd390dc4
0xEinCodes <0xEinCodes@gmail.com>
2023-12-05 15:01:03 -0600

    Write rough withdraw() logic

--- src/modules/SimpleSlippageRouter.sol
+++ src/modules/SimpleSlippageRouter.sol
@@ -29,24 +29,44 @@ contract SimpleSlippageRouter {
     error SimpleSlippageAdaptor__MinimumSharesUnmet(uint256 minimumShares, uint256 actualSharesQuoted);
 
     /**
+     * @notice Attempted to carry out tx with less than acceptable max shares.
+     * @param maxShares specified acceptable max shares amount
+     * @param actualSharesQuoted actual amount of shares to come from proposed tx
+     */
+    error SimpleSlippageAdaptor__MaxSharesSurpassed(uint256 maxShares, uint256 actualSharesQuoted);
+
+    /**
      * @notice deposits assets into specified cellar w/ minimumShares expected and deadline specified
-     * @param cellar specified cellar to deposit assets into
-     * @param assets amount of cellar base assets to deposit
-     * @param minimumShares amount of shares required at miniimum from tx
-     * @param deadline block.timestamp that tx must be carried out by
+     * @param _cellar specified cellar to deposit assets into
+     * @param _assets amount of cellar base assets to deposit
+     * @param _minimumShares amount of shares required at min from tx
+     * @param _deadline block.timestamp that tx must be carried out by
      */
     function deposit(Cellar _cellar, uint256 _assets, uint256 _minimumShares, uint64 _deadline) public {
         if (block.timestamp > _deadline) revert SimpleSlippageAdaptor__ExpiredDeadline(_deadline);
         _cellar.asset().safeTransferFrom(msg.sender, address(this), _assets);
-        uint256 shares = cellar.deposit(_assets, msg.sender);
+        uint256 shares = _cellar.deposit(_assets, msg.sender);
         if (shares < _minimumShares) revert SimpleSlippageAdaptor__MinimumSharesUnmet(_minimumShares, shares);
     }
 
     /**
-     * @notice withdraws assets in return for shares to be redeemed.
-     * @dev
+     * @notice withdraws assets as long as tx returns more than minimumAssets and is done before deadline.
+     * @param _cellar specified cellar to deposit assets into
+     * @param _assets amount of cellar base assets to deposit
+     * @param _maxShares max amount of shares to redeemed from tx
+     * @param _deadline block.timestamp that tx must be carried out by
      */
-    function withdraw() public {}
+    function withdraw(Cellar _cellar, uint256 _assets, uint256 _maxShares, uint64 _deadline) public {
+        if (block.timestamp > _deadline) revert SimpleSlippageAdaptor__ExpiredDeadline(_deadline);
+
+        uint256 shares = cellar.previewWithdraw(_assets);
+        if (shares > _maxShares) revert SimpleSlippageAdaptor__MaxSharesSurpassed(_maxShares, shares);
+
+        ERC20 cellarToken = ERC20(address(_cellar));
+
+        cellarToken.safeTransferFrom(msg.sender, address(this), shares);
+        uint256 shares = _cellar.withdraw(_assets, msg.sender, address(this));
+    }
 
     function mint() public {}
 
