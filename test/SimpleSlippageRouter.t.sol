04e2adb64c9fe2b2e4d8f9125179f8bbdf1e400e
0xEinCodes <0xEinCodes@gmail.com>
2023-12-06 13:23:59 -0600

    Finish rough test code w/ TODOs

--- test/SimpleSlippageRouter.t.sol
+++ test/SimpleSlippageRouter.t.sol
@@ -133,7 +133,6 @@ contract SimpleSlippageRouterTest is MainnetStarterTest, AdaptorHelperFunctions 
     }
 
     // test withdrawing using SimpleSlippageRouter
-    // TODO: debug
     function testWithdraw(uint256 assets) external {
         assets = bound(assets, 1e6, 100_000e6);
 
@@ -217,7 +216,6 @@ contract SimpleSlippageRouterTest is MainnetStarterTest, AdaptorHelperFunctions 
     }
 
     // test redeeming using SimpleSlippageRouter
-    // TODO: debug
     function testRedeem(uint256 assets) external {
         assets = bound(assets, 1e6, 100_000e6);
 
@@ -335,11 +333,11 @@ contract SimpleSlippageRouterTest is MainnetStarterTest, AdaptorHelperFunctions 
 
         // manipulate back so the deposit should resolve.
         deal(address(USDC), address(cellar), originalBalance);
-        simpleSlippageRouter.deposit(cellar, deposit1, minShares1, deadline1); // TODO: make sure this is good
+        simpleSlippageRouter.deposit(cellar, deposit1, minShares1, deadline1);
     }
 
-    function testWithdrawMaxSharesSurpassed() external {
-        // test revert in withdraw()
+    // TODO: underflow/overflow bug
+    function testWithdrawMaxSharesSurpassed(uint256 assets) external {
         assets = bound(assets, 1e6, 100_000e6);
 
         // deal USDC assets to test contract
@@ -347,14 +345,17 @@ contract SimpleSlippageRouterTest is MainnetStarterTest, AdaptorHelperFunctions 
         deposit1 = assets / 2;
         minShares1 = deposit1;
         deadline1 = uint64(block.timestamp + 1 days);
-
+        // deposit half using the SSR
         simpleSlippageRouter.deposit(cellar, deposit1, minShares1, deadline1);
 
-        // manipulate cellar to have lots of shares and thus not a 1:1 ratio anymore for withdrawals
-        uint256 originalBalance = USDC.balanceOf(address(cellar));
-        deal(address(USDC), address(cellar), assets * 10);
-        uint256 quoteShares = cellar.previewDeposit(assets);
+        // withdraw a quarter using the SSR
+        uint256 withdraw1 = deposit1 / 2;
+        uint256 maxShares1 = withdraw1; // assume 1:1 USDC:Shares shareprice
 
+        // manipulate cellar to have lots of shares and thus not a 1:1 ratio anymore for withdrawals
+        uint256 originalBalance = cellar.balanceOf(address(cellar));
+        deal(address(cellar), address(cellar), assets * 10);
+        uint256 quoteShares = cellar.previewWithdraw(withdraw1);
         vm.expectRevert(
             bytes(
                 abi.encodeWithSelector(
@@ -363,20 +364,89 @@ contract SimpleSlippageRouterTest is MainnetStarterTest, AdaptorHelperFunctions 
                     quoteShares
                 )
             )
-        ); // TODO: bug - hard to outline what outline what actual shares reported in error will be because it relies on having a token transfer first in the actual deposit() function.
+        );
+        simpleSlippageRouter.withdraw(cellar, withdraw1, maxShares1, deadline1);
+
+        // manipulate back so the deposit should resolve.
+        deal(address(cellar), address(cellar), originalBalance);
+        simpleSlippageRouter.withdraw(cellar, withdraw1, maxShares1, deadline1);
     }
 
-    function testMintMinimumSharesSurpassed() external {
+    function testMintMinimumSharesSurpassed(uint256 assets) external {
         // test revert in mint()
+        assets = bound(assets, 1e6, 100_000e6);
+
+        // deal USDC assets to test contract
+        deal(address(USDC), address(this), assets);
+        deposit1 = assets / 2;
+        minShares1 = deposit1;
+        deadline1 = uint64(block.timestamp + 1 days);
+
+        // manipulate cellar to have lots of USDC and thus not a 1:1 ratio anymore for shares
+        uint256 originalBalance = USDC.balanceOf(address(cellar));
+        deal(address(USDC), address(cellar), assets * 10);
+        uint256 quotedAssetAmount = cellar.previewMint(minShares1);
+
+        // mint with half of the assets using the SSR
+        vm.expectRevert(
+            bytes(
+                abi.encodeWithSelector(
+                    SimpleSlippageRouter.SimpleSlippageAdaptor__MintMinimumSharesSurpassed.selector,
+                    minShares1,
+                    deposit1,
+                    quotedAssetAmount
+                )
+            )
+        );
+        simpleSlippageRouter.mint(cellar, deposit1, minShares1, deadline1);
+
+        // manipulate back so the mint should resolve.
+        deal(address(USDC), address(cellar), originalBalance);
+        simpleSlippageRouter.mint(cellar, deposit1, minShares1, deadline1);
     }
 
-    function testRedeemMaxSharesSurpassed() external {
+    // TODO: underflow/overflow bug
+    function testRedeemMaxSharesSurpassed(uint256 assets) external {
         // test revert in redeem()
+        assets = bound(assets, 1e6, 100_000e6);
+
+        // deal USDC assets to test contract
+        deal(address(USDC), address(this), assets);
+        deposit1 = assets / 2;
+        minShares1 = deposit1;
+        deadline1 = uint64(block.timestamp + 1 days);
+        // deposit half using the SSR
+        simpleSlippageRouter.deposit(cellar, deposit1, minShares1, deadline1);
+
+        // redeem half of the shares test contract has using the SSR
+        uint256 withdraw1 = deposit1 / 2;
+        uint256 maxShares1 = withdraw1; // assume 1:1 USDC:Shares shareprice
+
+        ///
+        // manipulate cellar to have lots of shares and thus not a 1:1 ratio anymore for shares
+        uint256 originalBalance = cellar.balanceOf(address(cellar));
+        deal(address(cellar), address(cellar), assets * 10);
+        uint256 quotedAssetAmount = cellar.previewRedeem(maxShares1);
+        vm.expectRevert(
+            bytes(
+                abi.encodeWithSelector(
+                    SimpleSlippageRouter.SimpleSlippageAdaptor__RedeemMaxSharesSurpassed.selector,
+                    maxShares1,
+                    withdraw1,
+                    quotedAssetAmount
+                )
+            )
+        );
+        simpleSlippageRouter.redeem(cellar, withdraw1, maxShares1, deadline1); 
+
+        // manipulate back so the deposit should resolve.
+        deal(address(cellar), address(cellar), originalBalance);
+        simpleSlippageRouter.redeem(cellar, withdraw1, maxShares1, deadline1); 
     }
 
     // ========================================= INTEGRATION TEST =========================================
 
-    // Test the deposit function combined with the withdraw, mint, and redeem functions. This would have multiple users in it. It's a more full integration test.
+    // TODO: Test the deposit function combined with the withdraw, mint, and redeem functions. This would have multiple users in it. It's a more full integration test.
 
     //============================================ Helper Functions ===========================================
 
