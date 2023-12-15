d36c58534a3e761e3df2f43df39e3bc2e974c2cd
crispymangoes <crispymangoes@gmail.com>
2023-12-15 12:46:54 -0600

    Hash out mitigation for if the sequencer goes down

---
+++ src/modules/price-router/permutations/SequencerPriceRouter.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.21;
+
+import { PriceRouter, Registry, ERC20, IChainlinkAggregator } from "src/modules/price-router/PriceRouter.sol";
+
+contract SequencerPriceRouter is PriceRouter {
+    IChainlinkAggregator internal immutable sequencerUptimeFeed;
+
+    uint256 internal immutable gracePeriod;
+
+    error SequencerPriceRouter__SequencerDown();
+    error SequencerPriceRouter__GracePeriodNotOver();
+
+    constructor(
+        address _sequencerUptimeFeed,
+        uint256 _gracePeriod,
+        address newOwner,
+        Registry _registry,
+        ERC20 _weth
+    ) PriceRouter(newOwner, _registry, _weth) {
+        sequencerUptimeFeed = IChainlinkAggregator(_sequencerUptimeFeed);
+        gracePeriod = _gracePeriod;
+    }
+
+    /**
+     * @notice Layer 2 chains that use sequencers, can have the sequencer go down. If this happens we do not want
+     *         to price assets, as the datafeeds could be stale, and need to be updated.
+     */
+    function _runPreFlightCheck() internal view override {
+        (
+            ,
+            /*uint80 roundID*/ int256 answer,
+            uint256 startedAt /*uint256 updatedAt*/ /*uint80 answeredInRound*/,
+            ,
+
+        ) = sequencerUptimeFeed.latestRoundData();
+
+        // Answer == 0: Sequencer is up
+        // Answer == 1: Sequencer is down
+        if (answer == 1) {
+            revert SequencerPriceRouter__SequencerDown();
+        }
+
+        // Make sure the grace period has passed after the
+        // sequencer is back up.
+        uint256 timeSinceUp = block.timestamp - startedAt;
+        if (timeSinceUp <= gracePeriod) {
+            revert SequencerPriceRouter__GracePeriodNotOver();
+        }
+    }
+}
