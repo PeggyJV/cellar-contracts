8ea52a879a369f0fd0f94aaa4023e8b402eb9a45
crispymangoes <crispymangoes@gmail.com>
2024-01-26 12:40:58 -0800

    Hash out idea

---
+++ src/modules/withdraw-queue/IMultiAssetSolver.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.8.0;
+
+import { ERC20 } from "@solmate/tokens/ERC20.sol";
+
+interface IMultiAssetSolver {
+    function finishSolve(
+        bytes calldata runData,
+        address initiator,
+        ERC20 give,
+        ERC20 take,
+        uint256 sharesReceived,
+        uint256 assetApprovalAmount
+    ) external;
+}
