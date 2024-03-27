41412b948022e479a89133a73faf0a5befe31ee0
crispymangoes <crispymangoes@gmail.com>
2024-03-27 16:40:48 -0700

    Add Lido unstaking adaptor to deploy script

--- script/Mainnet/production/DeployWstEthExtension.s.sol
+++ script/Mainnet/production/DeployWstEthExtension.s.sol
@@ -2,6 +2,9 @@
 pragma solidity 0.8.21;
 
 import {WstEthExtension, PriceRouter} from "src/modules/price-router/Extensions/Lido/WstEthExtension.sol";
+import {LidoStakingAdaptor} from "src/modules/adaptors/Staking/LidoStakingAdaptor.sol";
+
+import {MainnetAddresses} from "test/resources/MainnetAddresses.sol";
 
 import "forge-std/Script.sol";
 import "forge-std/StdJson.sol";
@@ -10,11 +13,12 @@ import "forge-std/StdJson.sol";
  *  source .env && forge script script/Mainnet/production/DeployWstEthExtension.s.sol:DeployWstEthExtensionScript --with-gas-price 30000000000 --slow --broadcast --etherscan-api-key $ETHERSCAN_KEY --verify
  * @dev Optionally can change `--with-gas-price` to something more reasonable
  */
-contract DeployWstEthExtensionScript is Script {
+contract DeployWstEthExtensionScript is Script, MainnetAddresses {
     uint256 public privateKey;
 
     WstEthExtension public extension;
     PriceRouter public priceRouter = PriceRouter(0x693799805B502264f9365440B93C113D86a4fFF5);
+    LidoStakingAdaptor public lidoStakingAdaptor;
 
     function setUp() external {
         privateKey = vm.envUint("SEVEN_SEAS_PRIVATE_KEY");
@@ -25,6 +29,7 @@ contract DeployWstEthExtensionScript is Script {
         vm.startBroadcast(privateKey);
 
         extension = new WstEthExtension(priceRouter);
+        lidoStakingAdaptor = new LidoStakingAdaptor(address(WETH), 8, address(STETH), address(WSTETH), unstETH);
 
         vm.stopBroadcast();
     }
