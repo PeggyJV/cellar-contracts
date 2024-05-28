35e01cd1f9fe4c7f4d3b27051af07766f407d90b
crispymangoes <crispymangoes@gmail.com>
2024-05-28 09:35:28 -0700

    Finish Swell Simple Staking Adaptor Test

--- test/testAdaptors/SwellSimpleStakingAdaptor.t.sol
+++ test/testAdaptors/SwellSimpleStakingAdaptor.t.sol
@@ -64,6 +64,9 @@ contract SwellSimpleStakingAdaptorTest is MainnetStarterTest, AdaptorHelperFunct
         cellar.addPosition(0, weEthSwellPosition, abi.encode(true), false);
         vm.stopPrank();
 
+        uint256 ptEthInCellar = ERC20(pendleEethPt).balanceOf(address(cellar));
+        uint256 weETHInCellar = WEETH.balanceOf(address(cellar));
+
         // Move 100 eETH and 100 weETH into Swell Simple Staking.
         Cellar.AdaptorCall[] memory data = new Cellar.AdaptorCall[](1);
         bytes[] memory adaptorCalls = new bytes[](2);
@@ -85,20 +88,88 @@ contract SwellSimpleStakingAdaptorTest is MainnetStarterTest, AdaptorHelperFunct
             "Only assets in the weETH Simple Staking position should be withdrawable"
         );
 
-        // TODO use max available to deposit all assets.
+        assertApproxEqAbs(cellar.totalAssets(), initialAssets, 1, "The total assets in the cellar should be unchanged");
+
+        // Use max available to deposit all assets.
+        data = new Cellar.AdaptorCall[](1);
+        adaptorCalls = new bytes[](2);
+        adaptorCalls[0] = abi.encodeWithSelector(
+            SwellSimpleStakingAdaptor.depositIntoSimpleStaking.selector, pendleEethPt, type(uint256).max
+        );
+        adaptorCalls[1] = abi.encodeWithSelector(
+            SwellSimpleStakingAdaptor.depositIntoSimpleStaking.selector, WEETH, type(uint256).max
+        );
+
+        data[0] = Cellar.AdaptorCall({adaptor: address(swellSimpleStakingAdaptor), callData: adaptorCalls});
+        vm.startPrank(cellarOwner);
+        cellar.callOnAdaptor(data);
+        vm.stopPrank();
+
+        expectedWithdrawableAssets = priceRouter.getValue(WEETH, weETHInCellar, WETH);
+
+        assertEq(
+            cellar.totalAssetsWithdrawable(),
+            expectedWithdrawableAssets,
+            "Only assets in the weETH Simple Staking position should be withdrawable"
+        );
+
+        assertApproxEqAbs(cellar.totalAssets(), initialAssets, 1, "The total assets in the cellar should be unchanged");
 
         // Confirm all were deposited.
+        assertEq(ERC20(pendleEethPt).balanceOf(address(cellar)), 0, "Cellar should have no pt eETH.");
+        assertEq(WEETH.balanceOf(address(cellar)), 0, "Cellar should have no weETH.");
+
+        address user = vm.addr(1);
 
         // Have user withdraw and make sure they get weETH.
+        deal(address(cellar), user, 100e18, true);
+
+        vm.startPrank(user);
+        cellar.redeem(100e18, user, user);
+        vm.stopPrank();
+
+        uint256 expectedWeEthToUser = priceRouter.getValue(WETH, cellar.previewRedeem(100e18), WEETH);
+
+        assertApproxEqAbs(WEETH.balanceOf(user), expectedWeEthToUser, 1, "User should have received weETH");
 
         // Use withdraw to withdraw 100 of each
+        data = new Cellar.AdaptorCall[](1);
+        adaptorCalls = new bytes[](2);
+        adaptorCalls[0] =
+            abi.encodeWithSelector(SwellSimpleStakingAdaptor.withdrawFromSimpleStaking.selector, pendleEethPt, 100e18);
+        adaptorCalls[1] =
+            abi.encodeWithSelector(SwellSimpleStakingAdaptor.withdrawFromSimpleStaking.selector, WEETH, 100e18);
 
-        // confirm 100 was withdrawn
+        data[0] = Cellar.AdaptorCall({adaptor: address(swellSimpleStakingAdaptor), callData: adaptorCalls});
+        vm.startPrank(cellarOwner);
+        cellar.callOnAdaptor(data);
+        vm.stopPrank();
+
+        // Confirm 100 was withdrawn
+        assertEq(ERC20(pendleEethPt).balanceOf(address(cellar)), 100e18, "Cellar should have 100e18 pt eETH.");
+        assertEq(WEETH.balanceOf(address(cellar)), 100e18, "Cellar should have 100e18 weETH.");
 
         // Use max available to withdraw all assets
+        data = new Cellar.AdaptorCall[](1);
+        adaptorCalls = new bytes[](2);
+        adaptorCalls[0] = abi.encodeWithSelector(
+            SwellSimpleStakingAdaptor.withdrawFromSimpleStaking.selector, pendleEethPt, type(uint256).max
+        );
+        adaptorCalls[1] = abi.encodeWithSelector(
+            SwellSimpleStakingAdaptor.withdrawFromSimpleStaking.selector, WEETH, type(uint256).max
+        );
 
-        // confirm all assets were withdrawn
+        data[0] = Cellar.AdaptorCall({adaptor: address(swellSimpleStakingAdaptor), callData: adaptorCalls});
+        vm.startPrank(cellarOwner);
+        cellar.callOnAdaptor(data);
+        vm.stopPrank();
 
-        // Add part where we stake everything with type uint256 max, then unstaking everyhting
+        // Confirm all assets were withdrawn
+        assertEq(ERC20(pendleEethPt).balanceOf(address(cellar)), ptEthInCellar, "Cellar should have starting pt eETH.");
+        assertEq(
+            WEETH.balanceOf(address(cellar)),
+            weETHInCellar - expectedWeEthToUser,
+            "Cellar should have starting weETH, minus withdrawn amount"
+        );
     }
 }
