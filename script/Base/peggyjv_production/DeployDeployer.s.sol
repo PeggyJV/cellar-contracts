4ff3562db8a38b2e3db9aa7a15480769323b7729
otsalex <alexots13@gmail.com>
2024-11-06 18:56:53 +0200

    Work on Base infra

---
+++ script/Base/peggyjv_production/DeployDeployer.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.21;
+
+import { Deployer } from "src/Deployer.sol";
+
+import { BaseAddressesPeggyJV } from "test/resources/Base/BaseAddressesPeggyJV.sol";
+
+import "forge-std/Script.sol";
+
+/**
+ * @dev Run
+ *       source .env && forge script script/Base/peggyjv_production/DeployDeployer.s.sol:DeployDeployerScript --rpc-url $BASE_RPC_URL --with-gas-price 100000000 --broadcast --private-key $DEV0_PRIVATE_KEY —optimize —optimizer-runs 200
+
+ * @dev Optionally can change `--with-gas-price` to something more reasonable
+ */
+contract DeployDeployerScript is Script, BaseAddressesPeggyJV {
+
+    function run() external {
+        address[] memory deployers = new address[](2);
+        deployers[0] = dev0Address;
+
+        vm.startBroadcast();
+
+        Deployer deployer = new Deployer(dev0Address, deployers);
+        deployer.transferOwnership(dev0Address);
+        address deployerAddress = address(deployer);
+
+        vm.stopBroadcast();
+
+        console.log("Deployer deployed at:", deployerAddress);
+    }
+}
